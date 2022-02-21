08a6d05d5a820f13d671236d469610d226b7aab2
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2022-02-21 22:12:00 +0800

    Add markdown processor

--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
+using Blog;
+using ColorCode.Common;
+using ColorCode.Styling;
+using Markdig;
+using Markdown.ColorCode;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Blog;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+StyleDictionary? sd = StyleDictionary.DefaultLight;
+sd[ScopeName.PlainText].Background = "#FFF6F8FA";
+
+builder.Services.AddScoped(sp => new MarkdownPipelineBuilder()
+    .UseAdvancedExtensions()
+    .UseColorCode(sd)
+    //.UseSyntaxHighlighting()
+    .Build());
 
 await builder.Build().RunAsync();
