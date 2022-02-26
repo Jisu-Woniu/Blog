bf204a060332078dd1416539e46d528757337047
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2022-02-27 02:21:03 +0800

    Add JSON file to manage all articles.

---
+++ Models/ArticleInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Blog.Models;
+
+[Serializable]
+public class ArticleInfo
+{
+    public string Title { get; set; } = null!;
+    public string Url { get; set; } = null!;
+    public DateTimeOffset PostTime { get; set; }
+
+    public static ArticleInfo? FromJson(JsonObject jsonInfo)
+    {
+        return jsonInfo.Deserialize<ArticleInfo>();
+    }
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
