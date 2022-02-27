ea3e0d430212330215ee4ece3be96f8af55e46b7
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2022-02-27 13:12:07 +0800

    Fix date format and render sequence

---
+++ FormatExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog;
+
+public static class FormatExtension
+{
+    public static string SiteFormat(this DateTimeOffset dateTimeOffset)
+    {
+        return dateTimeOffset.LocalDateTime.ToString("yyyy 年 MM 月 dd 日 HH:mm");
+    }
+}
