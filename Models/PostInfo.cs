8a37d3daeb614b9f2c03ed50a822643cf427177e
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2022-03-04 12:36:01 +0800

    Remove unused files and add repost indicator.

--- Models/PostInfo.cs
+++ Models/PostInfo.cs
@@ -9,6 +9,15 @@ public class PostInfo
     public string Title { get; set; } = null!;
     public string UrlTitle { get; set; } = null!;
     public DateTimeOffset PostTime { get; set; }
+    public RepostInfo? Repost { get; set; }
+
+    public class RepostInfo
+    {
+        public string? OriginalAuthor { get; set; }
+        public Uri OriginalUrl { get; set; } = null!;
+        public string? License { get; set; }
+        public Uri? LicenseUrl { get; set; }
+    }
 
     public static PostInfo? FromJson(JsonObject jsonInfo)
     {
