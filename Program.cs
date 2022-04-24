cc6deb1d56f66fa0f6acecdd70a6a4b4760112df
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2022-04-24 19:38:34 +0800

    Refine links page.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using ColorCode.Styling;
 using Markdig;
 using Markdown.ColorCode;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Fast.Components.FluentUI;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +12,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(
     _ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }
 );
+
+builder.Services.AddFluentUIComponents();
 
 builder.Services.AddScoped(
     _ =>
