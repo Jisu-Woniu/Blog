b46cadd7dda8599f0ffb8b55f95d9082083e5241
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2022-05-25 01:25:55 +0800

    Update build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 curl -sSL https://dot.net/v1/dotnet-install.sh >dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
 ./dotnet6/dotnet --version
 ./dotnet6/dotnet workload install wasm-tools
 ./dotnet6/dotnet publish -c Release -o output
+cat > output/package.json << EOF
+{
+    "reactSnap": {
+        "puppeteer": {
+            "timeout": 50000
+        },
+        "source": "wwwroot",
+        "minifyHtml": {
+            "collapseWhitespace": true,
+            "removeComments": true
+        },
+        "puppeteerArgs": [
+            "--no-sandbox",
+            "--disable-setuid-sandbox"
+        ]
+    }
+}
+EOF
+npm install -g react-snap
+cd output
+react-snap
+cd ..
